caeffd9c4cfe508cb77d40fc18fcea0755caecc9
Guru Ramu <guru04041997@gmail.com>
2025-02-06 00:27:43 +0530

    feat: image update and package fix

--- src/api/mainEndpoints.ts
+++ src/api/mainEndpoints.ts
@@ -1,67 +1,67 @@
-import axios from './main';
+import { Profile, User } from '../constants/types';
 
 const signup = async (data: any) => {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const response = await axios.post('https://roseflix-backend.herokuapp.com/api/v1/signup', data);
-			resolve(response.data);
-		} catch (error) {
-			reject(error);
-		}
-	});
+    return new Promise((resolve) => {
+        const dummyResponse = { success: true, message: 'User signed up successfully', data };
+        resolve(dummyResponse);
+    });
 };
 
-
 const checkAvailability = async (data: any) => {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const response = await axios.post('https://roseflix-backend.herokuapp.com/api/v1/users/checkAvailability', data);
-			resolve(response.data);
-		} catch (error) {
-			reject(error);
-		}
-	});
+    return new Promise((resolve) => {
+        const isAvailable = data.value !== 'taken@example.com';
+        const dummyResponse = { success: true, isAvailable };
+        resolve(dummyResponse);
+    });
 };
 
 const signin = async (data: any) => {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const response = await axios.post('https://roseflix-backend.herokuapp.com/api/v1/signin', data);
-			resolve(response.data);
-		} catch (error) {
-			reject(error);
-		}
-	});
+    return new Promise((resolve) => {
+        const dummyResponse = { success: true, message: 'User signed in successfully', data: {
+            userDetails: {
+                _id: '123',
+                email: 'guru@gmail.com',
+                phone: '9994564326',
+                profiles: [
+                    {_id: '1', name: 'Profile 1', avatar: 'Avatar_02.png'},
+                    {_id: '1', name: 'Profile 3', avatar: 'Avatar_07.png'},
+                ]
+            }
+        }, authorization: "Bearer dummy" };
+        resolve(dummyResponse);
+    });
 };
 
 const upsertProfile = async (data: any) => {
 	return new Promise(async (resolve, reject) => {
 		try {
-			const response = await axios.post('https://roseflix-backend.herokuapp.com/api/v1/users/upsertProfile', data);
-			resolve(response.data);
+			// const response = await axios.post('https://roseflix-backend.herokuapp.com/api/v1/users/upsertProfile', data);
+			resolve(data);
 		} catch (error) {
 			reject(error);
 		}
 	});
 };
 
-const deleteProfile = async (data: any) => {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const response = await axios.post('https://roseflix-backend.herokuapp.com/api/v1/users/deleteProfile', data);
-			resolve(response.data);
-		} catch (error) {
-			reject(error);
-		}
-	});
+const deleteProfile = async (data: { profileId: string }) => {
+    return new Promise((resolve) => {
+        const dummyUser: User = {
+            _id: '123',
+            email: 'dummy@example.com',
+            phone: '1234567890',
+            profiles: []
+        };
+        const dummyResponse = { success: true, message: 'Profile deleted successfully', user: dummyUser };
+        resolve(dummyResponse);
+    });
 };
 
 const mainHttp = {
-	signup,
-	signin,
-	checkAvailability,
-	upsertProfile,
-	deleteProfile
+    signup,
+    signin,
+    checkAvailability,
+    upsertProfile,
+    deleteProfile
 };
 
 export default mainHttp;
